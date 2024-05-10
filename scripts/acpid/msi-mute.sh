35c3527c044784f13055273a50fa957d008500ae
TahedoranIX <aleix.valdivieso@gmail.com>
2024-05-10 23:22:47 +0200

    added scripts mute,downvol,upvol

--- scripts/acpid/msi-mute.sh
+++ scripts/acpid/msi-mute.sh
@@ -3,11 +3,11 @@
 archivo="/sys/class/leds/platform::mute/brightness"
 
 # Lee el valor actual del archivo
-#estado_actual=$(amixer sget Master | grep -i '\[on\]')
-estado_actual=$(cat "$archivo")
+estado_actual=$(amixer -D sysdefault:Generic_1 sget Master | grep -i '\[on\]')
+#estado_actual=$(cat "$archivo")
 
 # Cambia el estado entre 'on' y 'off'
-if [ "$estado_actual" == '0' ]; then
+if [ "$estado_actual" == '' ]; then
     nuevo_estado='1'
 else
     nuevo_estado='0'
